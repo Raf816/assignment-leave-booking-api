e14ba19d6a1869caf44c7e6669632a4a4180a0bb
615462781 <raf.ahmed@bt.com>
2025-05-06 19:14:58 +0100

    file struct changes

--- Assignment/index.ts
+++
@@ -1,7 +0,0 @@
-import { MyRouter } from "./caseStudyAPI/routes/MyRouter";
-import { Router } from "express";
-import { Server } from "./Server";
-
-const myRouter = new MyRouter(Router());
-const server = new Server(myRouter, 9900);
-server.start();
