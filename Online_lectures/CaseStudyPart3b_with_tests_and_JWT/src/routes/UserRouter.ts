cd1143ccf6028acda9a5815ffdb5aceab84f7bf1
615462781 <raf.ahmed@bt.com>
2025-05-09 18:09:32 +0100

    added security_lecture docs and removed some comments

--- Online_lectures/CaseStudyPart3b_with_tests_and_JWT/src/routes/UserRouter.ts
+++ Online_lectures/CaseStudyPart3b_with_tests_and_JWT/src/routes/UserRouter.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";  
+import { Router } from "express";  // Correctly import Router
 import { UserController } from '../controllers/UserController';
 
 export class UserRouter {
