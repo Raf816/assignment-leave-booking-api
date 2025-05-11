f8d5ab00cabfd70be3adc1a2f3abb9d8731fc188
615462781 <raf.ahmed@bt.com>
2025-05-11 10:21:13 +0100

    added LeaveRequest table to sql along with neccessary columns

--- Assignment/caseStudyAPI/src/data-source.ts
+++ Assignment/caseStudyAPI/src/data-source.ts
@@ -3,6 +3,7 @@ import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 import { User } from "./entity/User";
 import { Role } from "./entity/Role";
+import { LeaveRequest } from "./entity/LeaveRequest";
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
 export const AppDataSource = new DataSource({
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: true,
+    synchronize: false,
     logging: false,
-    entities: [Role, User]
+    entities: [Role, User, LeaveRequest]
 });
---
+++ Assignment/caseStudyAPI/src/entity/LeaveRequest.ts
@@ -0,0 +1,51 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { IsDateString, IsEnum, IsNotEmpty } from "class-validator";
+import { User } from "./User";
+
+export enum LeaveStatus {
+    PENDING = 'Pending',
+    APPROVED = 'Approved',
+    REJECTED = 'Rejected',
+    CANCELLED = 'Cancelled'
+  }
+  
+  @Entity()
+  export class LeaveRequest {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @ManyToOne(() => User, { nullable: false, eager: true })
+    user: User;
+  
+    @Column({ default: 'Annual Leave' })
+    leaveType: string;
+  
+    @Column()
+    @IsDateString({}, { message: 'Start date must be valid' })
+    startDate: string;
+  
+    @Column()
+    @IsDateString({}, { message: 'End date must be valid' })
+    endDate: string;
+  
+    @Column({
+      type: 'enum',
+      enum: LeaveStatus,
+      default: LeaveStatus.PENDING,
+    })
+    
+    @IsEnum(LeaveStatus)
+    status: LeaveStatus;
+  
+    @Column({ nullable: true })
+    reason: string;
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+  }
+
+
+
