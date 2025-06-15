8606ad320617fe510aa6f9ff15493cc0d20fb752
Raf816 <rafath816@gmail.com>
2025-06-15 15:57:53 +0100

    Delete oc/CaseStudyPart3b_SOLID directory

--- oc/CaseStudyPart3b_SOLID/src/types/IAuthenticatedJWTRequest.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Role } from "../entity/Role";
-
-export interface IAuthenticatedJWTRequest extends express.Request {
-    signedInUser?: {
-        email?: string;
-        role?: Role;
-    };
-}
